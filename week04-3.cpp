1984fb45ae620c05599a36ea6b31fc1b77369e0c
Rinka <85664152+rinkaaixu@users.noreply.github.com>
2022-09-30 12:02:07 +0800

    Add files via upload

---
+++ week04-3.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+for(int i=0; i<11; i++ )
+{printf("印出%d\n", i );
+}
+}
---
+++ week04-4.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0; i<11; i++ ) {
+        printf("印出%d\n", i );
+    }
+    for(int i=1; i<=4; i++ ) {
+        printf("人類的版本%d\n", i );
+    }
